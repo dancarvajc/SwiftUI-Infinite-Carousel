0a001cc7d7d0c4d7a60dd72b121b56b9bbf178ed
Daniel Carvajal <dancarvajal@udec.cl>
2022-08-01 12:17:39 -0400

    Initial commit

--- Sources/SwiftUIInfiniteCarousel/SwiftUIInfiniteCarousel.swift
+++
@@ -1,6 +0,0 @@
-public struct SwiftUIInfiniteCarousel {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/SwiftUIInfiniteCarouselTests/SwiftUIInfiniteCarouselTests.swift
+++ Tests/SwiftUIInfiniteCarouselTests/SwiftUIInfiniteCarouselTests.swift
@@ -2,10 +2,10 @@ import XCTest
 @testable import SwiftUIInfiniteCarousel
 
 final class SwiftUIInfiniteCarouselTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftUIInfiniteCarousel().text, "Hello, World!")
-    }
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(SwiftUIInfiniteCarousel().text, "Hello, World!")
+//    }
 }
